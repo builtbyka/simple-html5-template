c8817a79a3c46e97c5fdaf0f5c76f50db6dfae5f
Kerri-Anne <kerri-anne@piratalondon.com>
2013-02-04 22:24:13 +0000

    Initital commit with modernizr example and dropdown

---
+++ js/main.js
@@ -0,0 +1,85 @@
+(function ($) {
+
+	/* Create namespace */
+
+	var test = test || {};
+
+
+	/*
+		Core
+	*/
+
+	test.core = (function() {
+
+		var settings = {
+
+		};
+
+
+		function init() {
+
+			if ($('h1').length) {
+
+			test.example.init();
+
+			}
+
+		};
+
+		return {
+			init : init
+		};
+
+
+	}());
+
+
+	test.example = (function(){
+
+		alert("Working");
+
+		function init(){
+
+
+			bind();
+		};
+
+		function bind(){
+
+			if (Modernizr.csstransforms == 1) {
+				$('nav > ul li').mouseenter(function(){
+					$(this).find('ul').hide().slideDown();
+				});
+
+			}
+
+		// $('a').on('click', function(e){
+
+		// 	e.preventDefault();
+
+		// 	if (Modernizr.csstransforms) {
+		//         $('body').append('<p>This bind event lets you know this browser supports css transforms</p>');
+		//     }else{
+		//     	$('body').append('<p>This bind event lets you know this browser DOES NOT support css transforms cos its naff</p>');
+		//     }
+
+		// });
+
+		};
+
+		return{
+			init: init
+		};
+
+
+	})();
+
+
+	$(document).ready(function () {
+
+		test.core.init();
+
+	});
+
+}(jQuery));
+
